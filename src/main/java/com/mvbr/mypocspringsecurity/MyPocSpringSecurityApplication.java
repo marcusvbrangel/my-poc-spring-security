fe84994e34f4e235bfc68d591e60f69087822548
Marcus Rangel <marcus.vbrangel@gmail.com>
2025-05-22 16:44:08 -0300

    chore: initial commit

---
+++ src/main/java/com/mvbr/mypocspringsecurity/MyPocSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mvbr.mypocspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPocSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPocSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvbr/mypocspringsecurity/MyPocSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvbr.mypocspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPocSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
